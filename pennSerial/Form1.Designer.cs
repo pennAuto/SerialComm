ecd46303d902ea9fdf89f6dfb3a3826d6c51e68a
pennAuto <wprmead@live.co.uk>
2017-06-02 15:52:24 +0100

    Add project files.

---
+++ pennSerial/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace pennSerial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbBaudRate = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.grpSend = new System.Windows.Forms.GroupBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.grpReceive = new System.Windows.Forms.GroupBox();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.btnClosePort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbPortNames = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btnOpenPort = new System.Windows.Forms.Button();
+            this.grpSend.SuspendLayout();
+            this.grpReceive.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbBaudRate
+            // 
+            this.cmbBaudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBaudRate.FormattingEnabled = true;
+            this.cmbBaudRate.Items.AddRange(new object[] {
+            "9600",
+            "115200"});
+            this.cmbBaudRate.Location = new System.Drawing.Point(306, 40);
+            this.cmbBaudRate.Name = "cmbBaudRate";
+            this.cmbBaudRate.Size = new System.Drawing.Size(188, 28);
+            this.cmbBaudRate.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(584, 40);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(135, 28);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // grpSend
+            // 
+            this.grpSend.Controls.Add(this.btnSend);
+            this.grpSend.Controls.Add(this.txtSend);
+            this.grpSend.Location = new System.Drawing.Point(37, 119);
+            this.grpSend.Name = "grpSend";
+            this.grpSend.Size = new System.Drawing.Size(248, 244);
+            this.grpSend.TabIndex = 3;
+            this.grpSend.TabStop = false;
+            this.grpSend.Text = "Send Code";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Location = new System.Drawing.Point(110, 187);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(132, 42);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Enabled = false;
+            this.txtSend.Location = new System.Drawing.Point(6, 25);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(236, 156);
+            this.txtSend.TabIndex = 0;
+            // 
+            // grpReceive
+            // 
+            this.grpReceive.Controls.Add(this.btnReceive);
+            this.grpReceive.Controls.Add(this.txtReceive);
+            this.grpReceive.Location = new System.Drawing.Point(307, 119);
+            this.grpReceive.Name = "grpReceive";
+            this.grpReceive.Size = new System.Drawing.Size(248, 244);
+            this.grpReceive.TabIndex = 3;
+            this.grpReceive.TabStop = false;
+            this.grpReceive.Text = "Receive Code";
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Enabled = false;
+            this.btnReceive.Location = new System.Drawing.Point(110, 187);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(132, 42);
+            this.btnReceive.TabIndex = 1;
+            this.btnReceive.Text = "Receive";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Enabled = false;
+            this.txtReceive.Location = new System.Drawing.Point(6, 25);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.ReadOnly = true;
+            this.txtReceive.Size = new System.Drawing.Size(236, 156);
+            this.txtReceive.TabIndex = 0;
+            // 
+            // btnClosePort
+            // 
+            this.btnClosePort.Location = new System.Drawing.Point(606, 229);
+            this.btnClosePort.Name = "btnClosePort";
+            this.btnClosePort.Size = new System.Drawing.Size(114, 71);
+            this.btnClosePort.TabIndex = 4;
+            this.btnClosePort.Text = "Close Port";
+            this.btnClosePort.UseVisualStyleBackColor = true;
+            this.btnClosePort.Click += new System.EventHandler(this.btnClosePort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Port Names";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(580, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Status";
+            // 
+            // cmbPortNames
+            // 
+            this.cmbPortNames.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPortNames.FormattingEnabled = true;
+            this.cmbPortNames.Location = new System.Drawing.Point(43, 40);
+            this.cmbPortNames.Name = "cmbPortNames";
+            this.cmbPortNames.Size = new System.Drawing.Size(188, 28);
+            this.cmbPortNames.TabIndex = 1;
+            // 
+            // btnOpenPort
+            // 
+            this.btnOpenPort.Location = new System.Drawing.Point(605, 144);
+            this.btnOpenPort.Name = "btnOpenPort";
+            this.btnOpenPort.Size = new System.Drawing.Size(114, 71);
+            this.btnOpenPort.TabIndex = 6;
+            this.btnOpenPort.Text = "Open Port";
+            this.btnOpenPort.UseVisualStyleBackColor = true;
+            this.btnOpenPort.Click += new System.EventHandler(this.btnOpenPort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 418);
+            this.Controls.Add(this.btnOpenPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClosePort);
+            this.Controls.Add(this.grpReceive);
+            this.Controls.Add(this.grpSend);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cmbPortNames);
+            this.Controls.Add(this.cmbBaudRate);
+            this.Name = "Form1";
+            this.Text = "Serial Communication v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpSend.ResumeLayout(false);
+            this.grpSend.PerformLayout();
+            this.grpReceive.ResumeLayout(false);
+            this.grpReceive.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox cmbBaudRate;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox grpSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.GroupBox grpReceive;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.Button btnClosePort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbPortNames;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btnOpenPort;
+    }
+}
+
---
+++ pennSerial/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Management;
+
+
+namespace pennSerial
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailablePorts();
+        }
+        void getAvailablePorts()
+        {            
+            string[] ports = 
+                SerialPort.GetPortNames();
+            cmbPortNames.Items.AddRange(ports);   
+        }
+
+        private void btnOpenPort_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmbPortNames.Text == "" || cmbBaudRate.Text == "")
+                {
+                    txtReceive.Text = "Please select port settings";
+                }
+                else
+                {
+                    txtReceive.Text = "";
+                    serialPort1.PortName = cmbPortNames.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(cmbBaudRate.Text);
+                    serialPort1.RtsEnable = true;
+                    serialPort1.DtrEnable = true;
+                    serialPort1.Handshake = Handshake.None;
+                    serialPort1.Open();
+                    progressBar1.Value = 100;
+                    btnSend.Enabled = true;
+                    btnReceive.Enabled = true;
+                    txtSend.Enabled = true;
+                    btnOpenPort.Enabled = false;
+                    btnClosePort.Enabled = true;
+                 
+                }
+            }
+            catch(UnauthorizedAccessException)
+            {
+                txtReceive.Text = "Unauthorised Access";
+            }
+        }
+
+        private void btnClosePort_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            progressBar1.Value = 0;
+            btnSend.Enabled = false;
+            btnReceive.Enabled = false;
+            txtSend.Enabled = false ;
+            btnOpenPort.Enabled = true;
+            btnClosePort.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            txtReceive.Text = "";
+            //int sendVal = Convert.ToInt32(txtSend.Text);
+            byte[] buffer = new byte[1];
+            buffer[0] = 2;
+            serialPort1.Write(buffer, 0, 1);
+            txtSend.Text = "";
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            txtReceive.Text = "";
+            serialPort1.ReadTimeout = 3000;
+            try
+            {
+                txtReceive.Text = serialPort1.ReadLine();
+                //txtReceive.Text = serialPort1.ReadExisting();
+            }
+            catch (TimeoutException)
+            {
+                txtReceive.Text = "Timeout exception...";
+            }
+
+        }
+    }
+}
